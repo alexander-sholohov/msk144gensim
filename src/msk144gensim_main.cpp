0f5c69b5102cbd2613c51e49ca1641540b30e994
Alexander Sholohov <ra9yer@yahoo.com>
2022-05-14 15:51:15 +0700

    Initial commit

---
+++ src/msk144gensim_main.cpp
@@ -0,0 +1,362 @@
+//
+// Author: Alexander Sholohov <ra9yer@yahoo.com>
+//
+// License: MIT
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sched.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#if __GNUC__ > 7
+#include <cstddef>
+  typedef size_t fortran_charlen_t;
+#else
+  typedef int fortran_charlen_t;
+#endif
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <sstream>
+#include <cmath>
+
+
+extern "C" {
+  // --- Fortran routines ---
+
+    void genmsk_128_90_(char msg0[], 
+                        int* ichk, 
+                        char msgsent[],
+                        int* i4tone,
+                        int* itype,
+                        fortran_charlen_t, fortran_charlen_t);
+
+}
+
+
+struct Context
+{
+    float center_freq = 1500.0f;
+    float snrdb = 10.0f;
+    char message[80] = {"HELLO"};
+    int on_frames = 10;
+    int off_frames = 20;
+    int i4tone[144];
+};
+
+
+//------------------------------------------------------------
+static bool str2bool(const char* str)
+{
+    if (strcmp(str, "true") == 0)
+        return true;
+
+    if (strcmp(str, "1") == 0)
+        return true;
+
+    return false;
+}
+
+static void out_wav_16bit(const Context& ctx)
+{
+    // gen wav
+
+    const int nsps = 6;
+    const float baud = 2000.0f;
+    const float sps = baud * nsps; // 12000.0f;
+    const float two_pi = 8 * std::atan(1.0f);
+    const float dphi0 = two_pi * (ctx.center_freq - 0.25f * baud) / sps;
+    const float dphi1 = two_pi * (ctx.center_freq + 0.25f * baud) / sps;
+    float phi = 0.0f;
+    const float max_amp = 1000.0f;
+
+    while(true)
+    {
+        for(int i=0; i<ctx.on_frames; i++)
+        {
+            for(int j=0; j<144; j++)
+            {
+                const int ch = ctx.i4tone[j];
+                if(ch!=0 && ch!=1)
+                {
+                    throw std::runtime_error("wrong ch");
+                }
+
+                const float dphi = (ch == 0)? dphi0 : dphi1;
+
+                for(int k=0; k<nsps; k++)
+                {
+                    int v = std::cos(phi) * max_amp;
+                    char low = v & 0xff;
+                    char high = v >> 8;
+                    std::cout << low << high;
+                    // std::cout << "j:" << j 
+                    //     << " ch:" << ch 
+                    //     << " k:" << k 
+                    //     << " v:" << v 
+                    //     <<  std::endl;
+
+                    phi += dphi;
+                    if(phi > two_pi)
+                    {
+                        phi -= two_pi;
+                    }
+                }
+            }
+        }
+
+        // send silence
+        for(int i=0; i<ctx.off_frames; i++)
+        {
+            for(int j=0; j<144; j++)
+            {
+                for(int k=0; k<nsps; k++)
+                {
+                    unsigned char ch = 0;
+                    std::cout << ch << ch;
+                    // std::cout << "off" << std::endl;
+                }
+            }
+            
+        }
+    }
+
+}
+
+
+static void out_iq_8bit(const Context& ctx)
+{
+    const int symbols_per_second = 2000;
+    const int sdr_sample_rate = 2048000;
+    const int samples_per_symbol = sdr_sample_rate / symbols_per_second;
+    const int pp_len = samples_per_symbol * 2;
+
+    // std::cout << "pp_len = " << pp_len << std::endl;
+    // std::cout << "144 iq len = " << pp_len * 144  << std::endl;
+    // std::cout << "144*32 iq len = " << pp_len * 144 * 32 << std::endl;
+
+    // pp_len = 2048
+    // 144 iq len = 294912
+    // 144*32 iq len = 9437184
+
+
+    if (pp_len % 2 != 0)
+    {
+        throw std::runtime_error("wrong pp_len");
+    }
+
+    const int N72 = 144 / 2;
+
+    std::vector<float> pp(pp_len);
+    std::vector<float> q_res(N72 * pp_len);
+    std::vector<float> i_res(N72 * pp_len);
+
+    const float pi = 4 * std::atan(1.0f);
+    for (int i = 0; i < pp_len; i++)
+    {
+        pp[i] = std::sin(pi * i / pp_len);
+    }
+
+    // Q (half first)
+    for (int j = 0; j < pp_len / 2; j++)
+    {
+        q_res[j] = ctx.i4tone[0] * pp[pp_len / 2 + j];
+    }
+
+    // Q
+    for (int i = 1; i < N72 - 1; i++)
+    {
+        int base =  pp_len * i - pp_len / 2;
+        for (int j = 0; j < pp_len; j++)
+        {
+            q_res[base + j] = ctx.i4tone[2 * i] * pp[j];
+        }
+    }
+
+    // Q (half last)
+    {
+        int base = pp_len * N72 - pp_len / 2;
+        for (int j = 0; j < pp_len / 2; j++)
+        {
+            q_res[base + j] = ctx.i4tone[0] * pp[j];
+        }
+    }
+
+    // I
+    for (int i = 0; i < N72; i++)
+    {
+        int base = pp_len * i;
+        for (int j = 0; j < pp_len; j++)
+        {
+            i_res[base + j] = ctx.i4tone[2 * i + 1] * pp[j];
+        }
+    }
+
+    const float scale = 100.0;
+    while(true)
+    {
+        for(int i=0; i<ctx.on_frames; i++)
+        {
+            for (int i = 0; i < N72 * pp_len; i++)
+            {
+                char i_ch = static_cast<char>(i_res[i] * scale);
+                char q_ch = static_cast<char>(q_res[i] * scale);
+                std::cout << i_ch << q_ch;
+            }
+        }
+
+        // send silence
+        for(int i=0; i<ctx.off_frames; i++)
+        {
+            for (int i = 0; i < N72 * pp_len; i++)
+            {
+                char i_ch = 0;
+                char q_ch = 0;
+                std::cout << i_ch << q_ch;
+            }
+        }
+
+    }
+
+
+}
+
+//-------------------------------------------------------------------
+static void usage(void)
+{
+    std::ostringstream buf;
+    buf << "\nsk144gensim - msk144 generator. Produces infinite stdout audio stream - 16 bits signed, 12000 samples per second, mono.\n\n";
+    buf << "Usage:\t[--message= Message to send ]\n";
+    buf << "\t[--center-freq= Center frequency (default: 1500)]\n";
+    buf << "\t[--snr= snr in db (default: 10.0)]\n";
+    buf << "\t[--on-frames= ON frmaes (default: 10)]\n";
+    buf << "\t[--off-frames= ON frames (default: 20)]\n";
+    buf << "\t[--mode= 1-wav output; 2-IQ output (default: 1)]\n";
+    buf << "\t[--show-only= Print only text and bits to be send. (default: false)]\n";
+    buf << "\t[--help this text]\n";
+
+    std::cout << buf.str() << std::endl;
+
+    exit(1);
+}
+
+
+//------------------------------------------------------------
+int main(int argc, char** argv)
+{
+
+    static struct option long_options[] = {
+            {"help", no_argument, 0, 0}, // 0
+            {"message", required_argument, 0, 0}, // 1
+            {"center-freq", required_argument, 0, 0}, // 2
+            {"snr", required_argument, 0, 0}, // 3
+            {"on-frames", required_argument, 0, 0}, // 4
+            {"off-frames", required_argument, 0, 0}, // 5
+            {"show-only", required_argument, 0, 0}, // 6
+            {"mode", required_argument, 0, 0}, // 7
+            {0, 0, 0, 0}
+    };
+
+    Context ctx;
+    bool show_only = false;
+    int mode = 1; 
+
+
+    while (true)
+    {
+        int option_index = 0;
+
+        int c = getopt_long(argc, argv, "", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        if (c == 0)
+        {
+            switch (option_index)
+            {
+            case 0:
+                usage();
+                break;
+            case 1:
+                strcpy(ctx.message, optarg);
+                break;
+            case 2:
+                ctx.center_freq = atof(optarg);
+                break;
+            case 3:
+                ctx.snrdb = atof(optarg);
+                break;
+            case 4:
+                ctx.on_frames = atoi(optarg);
+                break;
+            case 5:
+                ctx.off_frames = atoi(optarg);
+                break;
+            case 6:
+                show_only = str2bool(optarg);
+                break;
+            case 7:
+                mode = atoi(optarg);
+                break;
+            default:
+                usage();
+            }
+        }
+    }
+
+
+    float sig = std::sqrt(2.0f) * std::pow(10.0f, 0.05f * ctx.snrdb);
+
+    char msgsent[200];
+    memset(msgsent, 0, sizeof(msgsent));
+
+    int ichk = 0;
+    int itype = 1;
+    genmsk_128_90_(ctx.message, &ichk, msgsent, ctx.i4tone, &itype, 37, 37);
+
+    if(show_only)
+    {
+        std::cout << "msg sent: '" << msgsent << "'" << std::endl;
+        std::cout << "i4tone[40] = " << ctx.i4tone[40] << std::endl;
+        std::cout << "i4tone:" << std::endl;
+        for(int i=0; i<144; i++)
+        {
+            std::cout << ctx.i4tone[i];
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "itype:" << itype << std::endl;
+        std::cout << "sig:" << sig << std::endl;
+        std::cout << "mode:" << mode << std::endl;
+        return 0;
+
+    }
+
+
+    if(mode == 1)
+    {
+        out_wav_16bit(ctx);
+    }
+    else if(mode == 2)
+    {
+        out_iq_8bit(ctx);
+    }
+    else 
+    {
+        throw std::runtime_error("Wrong mode");
+    }
+
+ 
+    std::cout << "Done." << std::endl;
+
+    return 0;
+}
